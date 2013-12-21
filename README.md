989f47abc28836c41e5d83e148d4fbe3ca1eddaf
nicholasyager <yager@nicholasyager.com>
2013-12-20 23:38:47 -0500

    Edited power and README. Removed weather.py from the github repo.

--- README.md
+++ README.md
@@ -7,3 +7,9 @@ Installation
 -----
 Place appropriate symbolic links in the home directory, .vim, and .xmonad folders,
 then restart X11 to verify that everything works.
+
+Scripts:
+-----
+ - *power.sh*: Read battery attributes and notify the user as per
+   applicable. This script requires libnotify and a notification daemon such as notify-osd.
+ - *volume.sh*: Read the computer volume via alsa and print it to the STDOUT.
--- power.sh
+++ power.sh
@@ -3,12 +3,15 @@
 stat=$(cat /sys/class/power_supply/BAT1/status)
 cap=$(cat /sys/class/power_supply/BAT1/capacity)
 
-if [ $cap -lt 10 ]; then
-    notify-send 'Power notification' 'You are low on power. Shutdown iminient!' --icon=dialog-warning
-fi
 
-if [ $cap -lt 5 ]; then
-    shutdown now;
+if [ $stat == "Discharging"]; then
+    if [ $cap -lt 20 ]; then
+        notify-send 'Power notification' 'You are low on power. Shutdown iminient!' --icon=dialog-warning
+    fi
+
+    if [ $cap -lt 6 ]; then
+        /usr/bin/systemctl suspend 
+    fi
 fi
 
 echo $stat $cap%
