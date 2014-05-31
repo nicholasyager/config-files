d92f8065e5b2cdc96e4322bd0322a15e5822402a
nicholasyager <yager@nicholasyager.com>
2014-05-31 12:28:28 -0400

    Fixed power timing to be the number of hours as opposed to an absolute time.

--- power.sh
+++ power.sh
@@ -2,18 +2,52 @@
 
 stat=$(cat /sys/class/power_supply/BAT1/status)
 cap=$(cat /sys/class/power_supply/BAT1/capacity)
+energy=$(cat /sys/class/power_supply/BAT1/energy_now)
+currenttime=$(date +"%s")
+history=$(cat ~/.bin/power.history)
+
+# Parse the power history
+IFS=';' read -ra HIST <<< $history
+let iter=0
+for i in ${HIST[@]}; do
+    if [ $iter -eq 0 ]; then
+        histtime=$i
+    fi
+
+    if [ $iter -eq 1 ]; then
+        histenergy=$i
+    fi
+    let iter++
+done
 
 
-if [ $stat == "Discharging"]; then
+if [ $stat == "Discharging" ]; then
+
+    timechange=$(echo "$currenttime-$histtime" | bc -l )
+    energychange=$(echo "$histenergy-$energy" | bc -l)
+    rate=$( echo "$energychange / $timechange" | bc -l)
+    timeremaining=$( echo "18000 + $energy / $rate " | bc -l)
+    end=$(date --date="@$timeremaining" +"%H:%M:%S")
+
     if [ $cap -lt 20 ]; then
-        notify-send 'Power notification' 'You are low on power. Shutdown iminient!' --icon=dialog-warning
+        if [ $cap -gt 15 ]; then
+            notify-send "Low Power notification" "Your battery is down to $cap%." --icon=battery-low
+        fi
     fi
 
-    if [ $cap -lt 6 ]; then
-        /usr/bin/systemctl suspend 
+    if [ $cap -lt 15 ]; then
+        notify-send "Low Power notification" "Your battery is down to $cap%. Shutdown imminent!" --icon=battery-caution
     fi
+
+
+    if [ $cap -lt 9 ]; then
+        shutdown now
+    fi
+
+    echo "$currenttime;$energy" > power.history
+
 fi
 
-echo $stat $cap%
+echo $stat $cap% $end
 
 exit 0
